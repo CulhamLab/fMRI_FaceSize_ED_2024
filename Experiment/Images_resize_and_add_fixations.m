cf17122aae9f0e38a2b46f0f043c6de0eb238d2c
Kevin Stubbs <kmstubbs@gmail.com>
2024-12-11 21:47:15 -0500

    remove ball, update fix

--- Experiment/Images_resize_and_add_fixations.m
+++ Experiment/Images_resize_and_add_fixations.m
@@ -9,8 +9,15 @@ img_height = 1080;
 
 for eye = ["l" "r"]
     for distance = [51 64 80]
-        fix = imread(fol_fixation + sprintf("Fixation_D%02d_%s.png", distance, eye));
+        name = sprintf("Fixation_D%02d_%s.png", distance, eye);
+        fix = imread(fol_fixation + name);
         fgd = repmat(any(fix <= threshold,3),[1 1 3]);
+
+        % resize
+        fix_rs = imresize(fix, [img_height img_width]);
+        
+        % save
+        imwrite(fix_rs, fol + name);
 
         list = dir(fol_without_fixation + sprintf("*_D%02d_*%s.png", distance, eye));
         for file = list(:)'
--- Experiment/obsolete/generate_placeholder_images.m
+++ Experiment/obsolete/generate_placeholder_images.m
@@ -23,24 +23,24 @@ for eye = ["l" "r"]
     for distance = distances
         % Fixation
         y = (0.1 + ((find(distances==distance)-1)/2*0.8)) * img_height;
-        img = ones(img_height,img_width,3,"uint8") * 255;
-        switch eye
-            case "l"
-                img = insertText(img, [(0.4 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1], FontColor=[0 0 0]);
-            case "r"
-                img = insertText(img, [(0.5 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1], FontColor=[0 0 0]);
-        end
-        imwrite(img, fol_fixation + sprintf("Fixation_D%02d_%s.png", distance, eye));
-
-        % Ball
         img = ones(img_height,img_width,3,"uint8") * 128;
         switch eye
             case "l"
-                img = insertText(img, [(0.1 * img_width) y], "Ball " + distance, FontSize=font_size, TextBoxColor=colour_left, FontColor=font_colour);
+                img = insertText(img, [(0.4 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1]*(128/255), FontColor=[0 0 0]);
             case "r"
-                img = insertText(img, [(0.7 * img_width) y], "Ball " + distance, FontSize=font_size, TextBoxColor=colour_right, FontColor=font_colour);
+                img = insertText(img, [(0.5 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1]*(128/255), FontColor=[0 0 0]);
         end
-        imwrite(img, fol_without_fixation + sprintf("Ball_D%02d_%s.png", distance, eye));
+        imwrite(img, fol_fixation + sprintf("Fixation_D%02d_%s.png", distance, eye));
+
+        % % Ball
+        % img = ones(img_height,img_width,3,"uint8") * 128;
+        % switch eye
+        %     case "l"
+        %         img = insertText(img, [(0.1 * img_width) y], "Ball " + distance, FontSize=font_size, TextBoxColor=colour_left, FontColor=font_colour);
+        %     case "r"
+        %         img = insertText(img, [(0.7 * img_width) y], "Ball " + distance, FontSize=font_size, TextBoxColor=colour_right, FontColor=font_colour);
+        % end
+        % imwrite(img, fol_without_fixation + sprintf("Ball_D%02d_%s.png", distance, eye));
 
         % Faces
         for sz = sizes
