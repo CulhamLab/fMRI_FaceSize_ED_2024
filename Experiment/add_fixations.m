461b9a9d4d52208f6b3e8ecc284845548f62d6c8
Kevin Stubbs <kmstubbs@gmail.com>
2024-12-11 21:15:14 -0500

    separated the fixation and ball images pt2

--- Experiment/add_fixations.m
+++ Experiment/add_fixations.m
@@ -1,12 +1,15 @@
 fol = ".\Images\";
-fol_without_fixation = fol + "WithoutFixation\";
+fol_fixation = fol + "Fixation_To_Add\";
+fol_without_fixation = fol + "Stims_Without_Fixation\";
 
-for eye = ["L" "R"]
+threshold = 20;
+
+for eye = ["l" "r"]
     for distance = [51 64 80]
-        fix = imread(fol + sprintf("Fixation_D%02d_%s.png", distance, eye));
-        fgd = repmat(any(fix,3),[1 1 3]);
+        fix = imread(fol_fixation + sprintf("Fixation_D%02d_%s.png", distance, eye));
+        fgd = repmat(any(fix <= threshold,3),[1 1 3]);
 
-        list = dir(fol_without_fixation + sprintf("Face*_D%02d_S*_%s.png", distance, eye));
+        list = dir(fol_without_fixation + sprintf("*_D%02d_*%s.png", distance, eye));
         for file = list(:)'
             img = imread([file.folder filesep file.name]);
             img(fgd) = fix(fgd);
--- Experiment/obsolete/generate_placeholder_images.m
+++ Experiment/obsolete/generate_placeholder_images.m
@@ -1,7 +1,9 @@
 %%
 fol = ".\Images\";
 if ~exist(fol, "dir"), mkdir(fol); end
-fol_without_fixation = fol + "WithoutFixation\";
+fol_fixation = fol + "Fixation_To_Add\";
+if ~exist(fol_fixation, "dir"), mkdir(fol_fixation); end
+fol_without_fixation = fol + "Stims_Without_Fixation\";
 if ~exist(fol_without_fixation, "dir"), mkdir(fol_without_fixation); end
 
 %%
@@ -13,32 +15,42 @@ font_colour = [1 1 1];
 colour_left = [0 0 1];
 colour_right = [1 0 0];
 
-eyes = ["L" "R"];
+eyes = ["l" "r"];
 distances = [51 64 80];
 sizes = [20 25 31];
 
-for eye = ["L" "R"]
+for eye = ["l" "r"]
     for distance = distances
         % Fixation
         y = (0.1 + ((find(distances==distance)-1)/2*0.8)) * img_height;
-        img = zeros(img_height,img_width,3,"uint8");
+        img = ones(img_height,img_width,3,"uint8") * 255;
         switch eye
-            case "L"
-                img = insertText(img, [(0.4 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=colour_left, FontColor=font_colour);
-            case "R"
-                img = insertText(img, [(0.5 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=colour_right, FontColor=font_colour);
+            case "l"
+                img = insertText(img, [(0.4 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1], FontColor=[0 0 0]);
+            case "r"
+                img = insertText(img, [(0.5 * img_width) y], "Fixation " + distance, FontSize=font_size, TextBoxColor=[1 1 1], FontColor=[0 0 0]);
         end
-        imwrite(img, fol + sprintf("Fixation_D%02d_%s.png", distance, eye));
+        imwrite(img, fol_fixation + sprintf("Fixation_D%02d_%s.png", distance, eye));
+
+        % Ball
+        img = ones(img_height,img_width,3,"uint8") * 128;
+        switch eye
+            case "l"
+                img = insertText(img, [(0.1 * img_width) y], "Ball " + distance, FontSize=fsz, TextBoxColor=colour_left, FontColor=font_colour);
+            case "r"
+                img = insertText(img, [(0.7 * img_width) y], "Ball " + distance, FontSize=fsz, TextBoxColor=colour_right, FontColor=font_colour);
+        end
+        imwrite(img, fol_without_fixation + sprintf("Ball_D%02d_%s.png", distance, eye));
 
         % Faces
         for sz = sizes
             name = sprintf("Face%02d_D%02d_S%02d_%s.png", 1, distance, sz, eye);
-            img = zeros(img_height,img_width,3,"uint8");
+            img = ones(img_height,img_width,3,"uint8") * 128;
             fsz = ceil(font_size * 1.5 * (sz / max(sizes)));
             switch eye
-                case "L"
+                case "l"
                     img = insertText(img, [(0.1 * img_width) y], name, FontSize=fsz, TextBoxColor=colour_left, FontColor=font_colour);
-                case "R"
+                case "r"
                     img = insertText(img, [(0.7 * img_width) y], name, FontSize=fsz, TextBoxColor=colour_right, FontColor=font_colour);
             end
             imwrite(img, fol_without_fixation + name);
