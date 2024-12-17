cf44e492f580b9bb7dab38b9e6dd6449ed6c5aa7
Kevin Stubbs <kmstubbs@gmail.com>
2024-12-17 15:10:41 -0500

    Update Images_resize_and_add_fixations.m

--- Experiment/Images_resize_and_add_fixations.m
+++ Experiment/Images_resize_and_add_fixations.m
@@ -9,19 +9,24 @@ img_height = 1080;
 
 for eye = ["l" "r"]
     for distance = [51 64 80]
-        %name = sprintf("Fixation_D%02d_%s.png", distance, eye);
-        name = sprintf("fixation.png");
+        name = sprintf("Fixation_D%02d_%s.png", distance, eye);
+        % name = sprintf("fixation.png");
 
         [fix,~,alpha] = imread(fol_fixation + name);
-        fgd = repmat(alpha > 200, [1 1 3]);
+        fgd = repmat(alpha > 0, [1 1 3]);
 
-        % resize
-        fix_rs = fix;
-        fix_rs(~fgd) = 255; %white background
-        fix_rs = imresize(fix_rs, [img_height img_width]);
-        
-        % save
-        imwrite(fix_rs, fol + name);
+        % handle b&w
+        if size(fix,3)==1
+            fix = repmat(fix,[1 1 3]);
+        end
+
+        % % resize
+        % fix_rs = fix;
+        % fix_rs(~fgd) = 255; %white background
+        % fix_rs = imresize(fix_rs, [img_height img_width]);
+        % 
+        % % save
+        % imwrite(fix_rs, fol + name);
 
         list = dir(fol_without_fixation + sprintf("*_D%02d_*%s.png", distance, eye));
         for file = list(:)'
